fdc76b2299eaddec26dc7838b363578381c39a4e
stan055 <stanislav055@gmail.com>
2021-09-02 13:26:15 +0300

    delete

--- android/app/src/main/kotlin/com/stan055/osbb_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.stan055.osbb_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/app_state_enum.dart
+++
@@ -1,9 +0,0 @@
-enum ApplicationStateEnum {
-  MOBILE_FORM,
-  OTP_FORM,
-  USER_INFO_FORM,
-  LOADING,
-  LOGGEDIN,
-  LOGGEDOUT,
-  LOGO
-}
